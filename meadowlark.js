6840282c4670b1cec3454b6804189a2854f7f57a
Brad Nelson <bnelson13@gmail.com>
2020-04-23 07:19:36 -0600

    Created module for fortune function

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fortune = require('./lib/fortune.js');
 
 // set up handlebars view engine
 const handlebars = require('express-handlebars')
@@ -15,17 +16,8 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
-const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
-
 app.get('/about', function(req, res){
-    let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 // custom 404 page
